0d9134b05c143e929ad8ba82b706f963819a27d2
oliwiamichalak <oliwia.michalak@gmail.com>
2025-05-18 21:33:58 +0100

    add counting files and lines in file

--- Task1/get_info_about_dir.py
+++ Task1/get_info_about_dir.py
@@ -13,7 +13,7 @@ EXT_TO_LANG = {
   ".ino": "C/C++",
   ".py": "Python",
   ".pyw": "Python",
-  ".md": "MARKDOWMN",
+  ".md": "MARKDOWN",
   ".html": "HTML",
   ".htm": "HTML",
   ".css": "CSS",
@@ -25,12 +25,15 @@ EXT_TO_LANG = {
 }
 
 def count_lines(fname):
-  return 42
+  with open(fname, "rb") as f:  # open as binary due to bypass decoding error for simplicity
+    return len(f.readlines()) 
+  # alternatively f.read().split(b'\n') - more lines in split method as it counts empty lines on the end opf the files
 
 def get_stats(stats):
   # unknown_exts = set() # dict without values with unique keys
   langs = defaultdict(int)
   total_loc = 0
+  total_file_count = 0
 
       # if lang in langs:
       #   langs[lang] += loc
@@ -38,6 +41,12 @@ def get_stats(stats):
       #   langs[lang] = loc this is handled by defaultDict
 
   for path, dirs, files in os.walk(DIR_TO_SCAN):
+
+    path_elements = path.split(os.path.sep)
+    if any([x.startswith(".") for x in path_elements]): # any([0,0,0]) => false, but any([0,1,0]) => true 
+      print("EXCLUDED:", path)
+      continue
+
     for file in files:
       full_fname = f"{path}/{file}" # don't use os.path.join as it deletes everything before /
       _, ext = os.path.splitext(file) # alternatively use .rsplit(".", 1) - split after right dot, once
@@ -47,15 +56,17 @@ def get_stats(stats):
       if lang is None:
         # unknown_exts.add(ext)
         continue
-
+      
       loc = count_lines(full_fname)
       total_loc += loc
+      total_file_count += 1
 
       langs[lang] += loc
       
   # print(unknown_exts)
   stats["langs"] = langs
   stats["totalLoC"] = total_loc
+  stats["totalFiles"] = total_file_count
 
 def show_stats(stats):
   pprint(stats)
@@ -75,7 +86,7 @@ def main():
       show_stats(stats)
       time.sleep(1.5) # Seconds
   except KeyboardInterrupt:
-   print(" done!")
+    print("\n done!")
 
 
 
